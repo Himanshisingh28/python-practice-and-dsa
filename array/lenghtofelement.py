2c8c3d2c0630b09ff0a494ecf8f3eb3d37740245
Himanshi Singh <himanshikashyap200528@gmail.com>
2026-02-08 19:00:57 +0530

    Fix a bug

--- array/lenghtofelement.py
+++ array/lenghtofelement.py
@@ -1,6 +1,6 @@
 class Solution(object):
     def lengthOfLastWord(self, s):
        list1=s.split()
-       last=list1[len(list1)-1]
-       return len(last)
+       last1=list1[len(list1)-1]
+       return len(last1)
         

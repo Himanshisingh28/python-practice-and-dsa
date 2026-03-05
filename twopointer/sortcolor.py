64001690d79cb04736f1f1ca1d94692d7e50d61b
Himanshi Singh <himanshikashyap200528@gmail.com>
2026-03-05 21:30:59 +0530

    Add more practice questions

---
+++ twopointer/sortcolor.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def sortColors(self, nums):
+        start=0
+        mid=0
+        end=len(nums)-1
+        while mid<=end:
+            if nums[mid]==0:
+                nums[mid],nums[start]=nums[start],nums[mid]
+                start+=1
+                mid+=1
+            elif nums[mid]==1:
+                mid+=1
+            else:
+                nums[mid],nums[end]=nums[end],nums[mid]
+                end-=1
+        return nums       

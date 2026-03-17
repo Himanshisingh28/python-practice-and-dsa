74cce2eb40847c9fdb9722ccabba8b5512dede5e
Himanshi Singh <himanshikashyap200528@gmail.com>
2026-03-17 21:57:34 +0530

    Add new pattern of DSA and Add new practiced question

---
+++ linklist/linkedlistCycle.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def hasCycle(self, head):
+        slow=head
+        fast=head
+        while(fast!= None and fast.next!=None):
+            slow=slow.next
+            fast=fast.next.next
+            if fast==slow:
+                return True
+        return False

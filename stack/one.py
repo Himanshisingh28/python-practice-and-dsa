13623fdd6d7eba8b4b81d874fa9d560a3855aadd
Himanshi Singh <himanshikashyap200528@gmail.com>
2026-04-14 22:04:10 +0530

    learning Stack

---
+++ stack/one.py
@@ -0,0 +1,11 @@
+# Q1 what is stack.
+# stack = []
+
+# stack.append(10)  # push
+# stack.append(20)
+# stack.append(30)
+
+# print(stack.pop())  # pop → 30
+# print(stack[-1])    # peek → 20
+
+# 👉 Stack is a linear data structure that follows LIFO (Last In First Out) where insertion and deletion happen from one end called TOP.

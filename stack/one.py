06c7c0cf3f11a91f0f0801cdf28516f9a3751d3a
Himanshi Singh <himanshikashyap200528@gmail.com>
2026-04-15 21:50:10 +0530

    Revision of stack

--- stack/one.py
+++ stack/one.py
@@ -9,3 +9,19 @@
 # print(stack[-1])    # peek → 20
 
 # 👉 Stack is a linear data structure that follows LIFO (Last In First Out) where insertion and deletion happen from one end called TOP.
+
+# for reversing a string using stack
+def reverse_string(s):
+    stack = []
+    
+    # Push each character of the string onto the stack
+    for char in s:
+        stack.append(char)
+    
+    reversed_str = ''
+    
+    # Pop characters from the stack and build the reversed string
+    while stack:
+        reversed_str += stack.pop()
+    
+    return reversed_str

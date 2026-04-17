c2b2b0de163168e1ddadd57116f82e9273e711d6
Himanshi Singh <himanshikashyap200528@gmail.com>
2026-04-17 21:54:29 +0530

    remove extra text

--- stack/one.py
+++ stack/one.py
@@ -1,13 +1,11 @@
 def reverse_string(s):
     stack = []
     
-    # Push each character of the string onto the stack
     for char in s:
         stack.append(char)
     
     reversed_str = ''
     
-    # Pop characters from the stack and build the reversed string
     while stack:
         reversed_str += stack.pop()
     

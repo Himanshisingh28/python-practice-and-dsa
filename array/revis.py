8250adbcb95bef8e1a6861142f2ab4da880ddbb5
Himanshi Singh <himanshikashyap200528@gmail.com>
2026-04-20 21:13:11 +0530

    Add new practiced question

--- array/revis.py
+++ array/revis.py
@@ -6,3 +6,17 @@ stack.append(20)
 print(stack.pop())  # pop
 print(stack[-1])    # peek
 
+class Solution(object):
+    def merge(self, intervals):
+        intervals.sort()
+        res = [intervals[0]]
+
+        for i in range(1, len(intervals)):
+            start, end = intervals[i]
+            
+            if start <= res[-1][1]:
+                res[-1][1] = max(res[-1][1], end)
+            else:
+                res.append([start, end])
+        
+        return res

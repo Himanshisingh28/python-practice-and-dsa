98496561d2ae19039603126ed9fee56e401fbdc5
Himanshi Singh <himanshikashyap200528@gmail.com>
2026-04-22 21:41:31 +0530

    Add more information about arrays

--- array/squre_of_sorted.py
+++ array/squre_of_sorted.py
@@ -1,3 +1,4 @@
+# You’re given a sorted array (ascending). Return a new array containing the squares of each number, also sorted in ascending order.
 class Solution(object):
     def sortedSquares(self, nums):
         squre=[num**2 for num in nums]

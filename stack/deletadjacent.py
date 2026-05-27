d2c23e93942a18f34f48fa535bc041314e31250f
Himanshisingh28 <himanshikashyap200528@gmail.com>
2026-05-27 13:34:57 +0530

    fix a bug

--- stack/deletadjacent.py
+++ stack/deletadjacent.py
@@ -13,18 +13,3 @@ class Solution(object):
             res.append(ch*cnt)
         return "".join(res)
     
-
-class Solution(object):
-    def removeDuplicates(self, s, k):
-        stack=[]
-        for ch in s:
-            if stack and stack[-1][0]==ch:
-                stack[-1][1]+=1
-                if stack[-1][1]==k:
-                    stack.pop()
-            else:
-                stack.append([ch,1])
-        res=[]
-        for ch, cnt in stack:
-            res.append(ch*cnt)
-        return "".join(res)

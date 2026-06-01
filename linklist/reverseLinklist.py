993f17045207e3d6a00f65a913ff2069597de6c5
Himanshisingh28 <himanshikashyap200528@gmail.com>
2026-06-01 14:41:46 +0530

    add new practiced problem

---
+++ linklist/reverseLinklist.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def reverseList(self, head):
+        curr=head
+        prev=None
+
+        while curr is not None:
+            next_node=curr.next
+            curr.next=prev
+            prev=curr
+            curr=next_node
+        return prev
+
+        

c42b672e35e15f0209eb474b89eeb6aca2b55599
Himanshisingh28 <himanshikashyap200528@gmail.com>
2026-06-15 16:52:25 +0530

    Fix a bug

--- binarySearch/2dII.py
+++ binarySearch/2dII.py
@@ -7,3 +7,18 @@ class Solution(object):
                 if matrix[i][j]==target:
                     return True
         return False
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+
+        n=len(matrix)
+        m=len(matrix[0])
+        row=n-1
+        col=0
+        while row>=0 and col< m:
+            if matrix[row][col]==target:
+                return True
+            if matrix[row][col]<target:
+                col+=1
+            else:
+                row-=1
+        return False

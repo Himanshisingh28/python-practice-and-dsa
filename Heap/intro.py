56b5ffde1ed581f03f66556d35a6542391427637
Himanshisingh28 <himanshikashyap200528@gmail.com>
2026-06-21 19:06:13 +0530

    Inroduction of Heap

---
+++ Heap/intro.py
@@ -0,0 +1,10 @@
+# introduction to heap data structure
+# In python heap is min heap by defaulted
+import heapq
+
+from httpx import head
+
+heap = []
+
+heapq.heappush(heap, 3)
+heapq.heappop(head)
